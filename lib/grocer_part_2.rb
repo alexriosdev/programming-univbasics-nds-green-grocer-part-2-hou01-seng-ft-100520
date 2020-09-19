9d560371ee5f25a54e9ed6319a6a5f8fff539412
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-19 07:06:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,8 +5,20 @@ def apply_coupons(cart, coupons)
   #
   # REMEMBER: This method **should** update cart
   
-  new_cart = consolidate_cart(cart)
-  
+  # new_cart = consolidate_cart(cart)
+  coupons.each do |coupon|
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    count_is_big_enough_to_apply = item_with_coupon[:count] >= coupon[:num]
+    if count_is_big_enough_to_apply
+      cart << { item: "#{item_with_coupon[:item]} W/COUPON", 
+                price: coupon[:cost] / coupon[:num], 
+                clearance: item_with_coupon[:clearance],
+                count: coupon[:num]
+              }
+      item_with_coupon[:count] -= coupon[:num]
+    end
+  end
+  cart
   
 end
 
